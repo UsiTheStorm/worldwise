54cf9ff69a88c2ffb2ea48a719bc15575cbcf39c
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-09-02 13:11:29 +0100

    Define routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Homepage from './pages/Homepage';
+import Pricing from './pages/Pricing';
+import Product from './pages/Product';
 
 import './App.css';
 
 function App() {
-  return <div>HELLO</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/product" element={<Product />}></Route>
+        <Route path="/pricing" element={<Pricing />}></Route>
+
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
