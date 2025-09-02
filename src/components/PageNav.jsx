0ce8c471ef4b33bf426bc54b71e0b376b3a9ffbb
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-09-02 15:31:40 +0100

    Add logo component

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,10 +1,13 @@
 import { NavLink } from 'react-router-dom';
 
+import Logo from './Logo';
+
 import styles from './PageNav.module.css';
 
 function PageNav() {
   return (
     <nav className={styles.nav}>
+      <Logo />
       <ul>
         <li>
           <NavLink to="/">Homepage</NavLink>
