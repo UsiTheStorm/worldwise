ec1124c3db5e269581f29c402ccbb2241f111ec5
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-09-02 13:14:42 +0100

    Add page not found page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Homepage from './pages/Homepage';
+import PageNotFound from './pages/PageNotFound';
 import Pricing from './pages/Pricing';
 import Product from './pages/Product';
 
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Homepage />}></Route>
         <Route path="/product" element={<Product />}></Route>
         <Route path="/pricing" element={<Pricing />}></Route>
+        <Route path="*" element={<PageNotFound />}></Route>
 
       </Routes>
     </BrowserRouter>
