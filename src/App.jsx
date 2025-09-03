0147cef74370b79c6367c98df2ca7e3a85c6b1cd
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-09-03 15:24:12 +0100

    Add country data fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import CityList from './components/CityList';
@@ -10,7 +11,35 @@ import Pricing from './pages/Pricing';
 import Product from './pages/Product';
 import './App.css';
 
+const BASE_URL = 'http://localhost:3000';
+
 function App() {
+  const [cities, setCities] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        if (!res.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        const data = await res.json();
+        setCities(data);
+      }
+      catch (error) {
+        console.error(error);
+      }
+      finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
