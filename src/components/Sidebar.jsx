1fd01f68bb55ff6a472fc53e7e217d66c603ce33
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-12-04 12:50:48 +0000

    Fix import appearence

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,7 +4,9 @@ import { Outlet } from 'react-router-dom';
 
 import AppNav from './AppNav';
 import Logo from './Logo';
+
 import styles from './Sidebar.module.css';
+
 import SidebarFooter from './SidebarFooter';
 
 function Sidebar() {
