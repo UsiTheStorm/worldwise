a494a6f0eb79a39c31bc6f00cb5bd9c181941686
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-12-05 11:28:45 +0000

    Edit config

--- eslint.config.js
+++ eslint.config.js
@@ -1,35 +1,45 @@
-// .eslintrc.js
 import antfu from '@antfu/eslint-config';
+import perfectionist from 'eslint-plugin-perfectionist';
 
-export default antfu({
-  // Тип проекту: 'app' для звичайного веб/віте-проекту
-  type: 'app',
-  react: true,
+// Витягуємо лише правила з конфігурації Perfectionist
+const perfectionistRules = perfectionist.configs['recommended-natural'].rules;
 
-  // TypeScript вимикаємо, бо у нас JS
-  typescript: false,
+export default antfu(
+  {
+    formatters: true,
+    react: true,
+    stylistic: {
+      indent: 2,
+      quotes: 'single',
+      semi: true,
+    },
+    type: 'app',
 
-  // Форматери включаємо, бо класно мати uniform code style
-  formatters: true,
-
-  stylistic: {
-    indent: 2, // відступи у 2 пробіли
-    semi: true, // ставимо крапку з комою після кожного виразу
-    quotes: 'single', // одинарні лапки
+    typescript: false,
   },
-}, {
-  rules: {
-    'no-console': ['warn'], // попередження при console.log()
-    'antfu/no-top-level-await': ['off'], // можна юзати top-level await
-    'node/prefer-global/process': ['off'], // не обов'язково використовувати глобальний process
-    'node/no-process-env': ['error'], // заборона використовувати process.env напряму
-    // 'perfectionist/sort-imports': ['error', { tsconfigRootDir: '.' }], // сортування імпортів
-    // "unicorn/filename-case": ["error", { case: "kebabCase", ignore: ["README.md"] }], // файли у kebab-case
-    'react/jsx-one-expression-per-line': 'off',
-    'style/jsx-one-expression-per-line': 'off',
-    'style/jsx-curly-brace-presence': [
-      'error',
-      { props: 'never', children: 'never' },
-    ],
+
+  {
+    rules: {
+      // 1. Розгортаємо всі правила Perfectionist першими.
+      // Це замінює необхідність підключати об'єкт plugins
+      ...perfectionistRules,
+
+      'antfu/no-top-level-await': ['off'],
+      // 2. Вимикаємо конфліктуючі правила Antfu
+      'import/order': 'off',
+
+      
+      'no-console': ['warn'],
+      'node/no-process-env': ['error'],
+      'node/prefer-global/process': ['off'],
+      'react/jsx-one-expression-per-line': 'off',
+
+      'sort-imports': 'off',
+      'style/jsx-curly-brace-presence': [
+        'error',
+        { children: 'never', props: 'never' },
+      ],
+      'style/jsx-one-expression-per-line': 'off',
+    },
   },
-});
+);
