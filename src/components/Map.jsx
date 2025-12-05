8675e0f74c6e156a0a877685c3733ec8125543c7
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-12-05 15:25:41 +0000

    Implement position variables showing

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
-import { useSearchParams } from 'react-router-dom';
 import styles from './Map.module.css';
 
 function Map() {
-  useSearchParams();
+  const navigate = useNavigate();
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
+
   return (
-    <div className={styles.mapContainer}>Map</div>
+    <div className={styles.mapContainer} onClick={() => navigate('form')}>
+      <h1>Map</h1>
+      <h2>Position: {lat}, {lng}</h2>
+    </div>
   );
 }
 
