ccb0b0a8234d1a2d6d8f6d5a859b9bf1b978f8f5
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-12-05 11:30:06 +0000

    Update map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import { useSearchParams } from 'react-router-dom';
 import styles from './Map.module.css';
 
 function Map() {
+  useSearchParams();
   return (
     <div className={styles.mapContainer}>Map</div>
   );
