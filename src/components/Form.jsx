c53bf4d6d3d8e014efb7b43187545b9de06ebe29
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-12-16 16:13:10 +0000

    Add BackButton component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,9 @@
 // "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
 
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
+import BackButton from './BackButton';
 import Button from './Button';
 
 import styles from './Form.module.css';
@@ -16,7 +17,7 @@ export function convertToEmoji(countryCode) {
 }
 
 function Form() {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   const [cityName, setCityName] = useState('');
   const [country, setCountry] = useState('');
@@ -55,14 +56,15 @@ function Form() {
 
       <div className={styles.buttons}>
         <Button type="primary">Add</Button>
-        <Button
+        <BackButton />
+        {/* <Button
           onClick={(e) => {
             e.preventDefault();
             navigate(-1);
           }}
           type="back"
         >Back
-        </Button>
+        </Button> */}
       </div>
     </form>
   );
