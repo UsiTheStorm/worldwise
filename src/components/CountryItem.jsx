1731c9934fb87da0779f4f8cecc4820a2248a535
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-12-24 14:02:05 +0000

    Fix country card with long counry names

--- src/components/CountryItem.jsx
+++ src/components/CountryItem.jsx
@@ -1,10 +1,17 @@
 import styles from './CountryItem.module.css';
 
 function CountryItem({ country }) {
+  const isTruncated = country.country.length > 14;
+
   return (
     <li className={styles.countryItem}>
       <span>{country.emoji}</span>
-      <span>{country.country}</span>
+      <span
+        className={styles.countryName}
+        title={isTruncated ? country.country : ''}
+      >{country.country}
+      </span>
+
     </li>
   );
 }
